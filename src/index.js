750c87383c73b9890303491569aafad5eda6a5ca
Luis Eduardo <luiseperezp.10@gmail.com>
2020-11-12 01:37:22 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const messages = [
+    'Luis', 'Eduardo', 'Diego', 'Marlon', 'Laura', 'Jesus'
+];
+
+const randomMsg = () => {
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  console.log(message);
+};
+
+module.exports = { randomMsg };
